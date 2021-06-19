c2176e0643a33c7d0646a7e8c5fe80634b162347
ass77548921 <76846676+ass77548921@users.noreply.github.com>
2021-06-19 23:09:55 +0800

    Update cpu.js

--- api/cpu/controllers/cpu.js
+++ api/cpu/controllers/cpu.js
@@ -112,7 +112,7 @@
      const core = ctx.request.body.core;
      const socket_id = ctx.request.body.socket_id;
      const image_url = ctx.request.body.image_url;
-     constGHz = ctx.request.body.type;
+     const GHz = ctx.request.body.type;
      console.log(name, core, socket_id, image_url,GHz,id);
    
      const form_data = {
